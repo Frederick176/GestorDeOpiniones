4299c6df7a52060373efd53d3ef3e45a67bee823
Frederick176 <fhernandez-2023176@kinal.edu.gt>
2025-02-18 16:46:04 -0600

    Avanze del proyecto de practica

--- src/user/user.model.js
+++ src/user/user.model.js
@@ -10,5 +10,49 @@ const userSchema = Schema({
         type: String,
         required: [true, "Surname is required"],
         maxLength: [30, "Surname cannot exced 30 characteres"]
+    },
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: [true, "Email is required"],
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true, "Password is required"]
+    },
+    profilePicture:{
+        type: String
+    },
+    phone:{
+        type: String,
+        minLength: 8,
+        maxLength: 8,
+        required: true
+    },
+    role:{
+        type: String,
+        required: true,
+        enum: ["AUTOR_ROLE", "USER_ROLE"]
+    },
+    status:{
+        type: Boolean,
+        default: true
     }
+},
+{
+    versionKey: false,
+    timeStameps: true
 })
+
+userSchema.methods.toJSON = function(){
+    const {password, _id, ...usuario} = this.toObject()
+    usuario.uid = _id
+    return usuario
+}
+
+export default model("User", userSchema)
